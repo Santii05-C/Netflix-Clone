67d43338e2358de09c1a411ca674373faadb11ba
Santii05-C <santi.c.pgm@gmail.com>
2025-03-03 14:37:11 -0300

    last commit

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready ");
-});
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 app.listen(5000, () => {
   console.log("Server started at http://localhost:5000");
