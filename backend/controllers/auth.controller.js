19b2d5ea269e83570f01470e0bbef5b0024a82e5
Santii05-C <santi.c.pgm@gmail.com>
2025-03-04 14:25:13 -0300

    last commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import { User } from "../models/user.model.js";
+
 export async function signup(req, res) {
   try {
     const { email, password, username } = req.body;
@@ -7,8 +9,38 @@ export async function signup(req, res) {
         .status(400)
         .json({ success: false, message: "All fields are required" });
     }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ success: false, message: "Invalid email" });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: "Password must be at least 6 characters",
+      });
+    }
+
+    const existingUserByEmail = await User.findOne({ email: email });
+
+    if (existingUserByEmail) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Email already exists" });
+    }
+
+    const existingUserByUsername = await User.findOne({ username: username });
+
+    if (existingUserByUsername) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Username already exists" });
+    }
   } catch (error) {}
 }
+
 export async function login(req, res) {
   res.send("asdasdss1231231");
 }
