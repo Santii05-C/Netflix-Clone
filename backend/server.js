8ecbdc27f9772a9992ab046af507bd53caac5d09
Santii05-C <santi.c.pgm@gmail.com>
2025-03-04 10:58:21 -0300

    last commit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 
+dotenv.config();
+
 const app = express();
+
+console.log("MONGO_URI", process.env.MONGO_URI);
 
 app.use("/api/v1/auth", authRoutes);
 // app.use("/api/v1/movie", protectRoute, movieRoutes);
