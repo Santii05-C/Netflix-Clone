9cc1ae33989ddc83f64e394c5f378ae791a5677a
Santii05-C <santi.c.pgm@gmail.com>
2025-03-12 10:30:43 -0300

    last commit

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -37,7 +37,26 @@ export async function getMovieDetails(req, res) {
     );
     res.status(200).json({ success: true, content: data });
   } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 }
-//1:33
+
+export async function getSimilarMovie(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`
+    );
+    res.status(200).json({ success: true, similar: data.results });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -3,6 +3,7 @@ import {
   getTrendingMovie,
   getMovieTrailers,
   getMovieDetails,
+  getSimilarMovie,
 } from "../controllers/movie.controller.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/trending", getTrendingMovie);
 router.get("/:id/trailers", getMovieTrailers);
 router.get("/:id/details", getMovieDetails);
+router.get("/:id/similar", getSimilarMovie);
 
 export default router;
