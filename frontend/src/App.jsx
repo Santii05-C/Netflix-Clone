b8f7bddd8324e5864c6114af92313484d2585c72
Santii05-C <santi.c.pgm@gmail.com>
2025-03-29 20:19:51 -0300

    last commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
 function App() {
   return (
-    <h1 className="text-5xl font-bold underline bg-red-500">Hello world!</h1>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+    </Routes>
   );
 }
 
