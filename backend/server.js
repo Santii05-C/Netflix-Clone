0b1513792e32636ae2930caf96fa9b8941e1f160
Santii05-C <santi.c.pgm@gmail.com>
2025-03-29 14:33:15 -0300

    last commit

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
@@ -19,12 +20,9 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
-// app.use("/api/v1/search", protectRoute, searchRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
   connectDB();
 });
-
-////https://faphouse.com/es/videos/xG9mii
-//1:57
