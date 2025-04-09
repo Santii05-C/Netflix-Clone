99387c737284fa3b1d2fd9752092480e638e250e
Santii05-C <santi.c.pgm@gmail.com>
2025-04-09 13:50:15 -0300

    last commit

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ChevronRight } from "lucide-react";
 const AuthScreen = () => {
   const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    navigate("/signup?email=" + email);
+  };
+
   return (
     <div className="hero-bg relative">
       {/* Navbar */}
@@ -27,7 +34,10 @@ const AuthScreen = () => {
           Ready to watch? Enter your email to create or restart your membership.
         </p>
 
-        <form className="flex flex-col md:flex-row gap-4 w-1/2">
+        <form
+          className="flex flex-col md:flex-row gap-4 w-1/2"
+          onSubmit={handleFormSubmit}
+        >
           <input
             type="email"
             placeholder="Email address"
@@ -126,8 +136,74 @@ const AuthScreen = () => {
           </div>
         </div>
       </div>
+
+      {/* separator */}
+      <div className="h-2 w-full bg-[#232323]" aria-hidden="true" />
+
+      {/* 3rd section */}
+      <div className="py-10 bg-black text-white">
+        <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
+          {/* left side */}
+          <div className="flex-1 text-center md:text-left">
+            <h2 className="text-4xl md:text-5xl font-extrabold mb-4">
+              Watch everywhere
+            </h2>
+            <p className="text-lg md:text-xl">
+              Stream unlimited movies and TV shows on your phone, tablet,
+              laptop, and TV.
+            </p>
+          </div>
+
+          {/* right side */}
+          <div className="flex-1 relative overflow-hidden">
+            <img
+              src="/device-pile.png"
+              alt="Device image"
+              className="mt-4 z-20 relative"
+            />
+            <video
+              className="absolute top-2 left-1/2 -translate-x-1/2  h-4/6 z-10
+               max-w-[63%] 
+              "
+              playsInline
+              autoPlay={true}
+              muted
+              loop
+            >
+              <source src="/video-devices.m4v" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+      </div>
+
+      {/* separator */}
+      <div className="h-2 w-full bg-[#232323]" aria-hidden="true" />
+
+      {/* 4th section*/}
+      <div className="py-10 bg-black text-white">
+        <div
+          className="flex max-w-6xl mx-auto items-center justify-center flex-col-reverse md:flex-row
+           px-4 md:px-2
+        "
+        >
+          {/* left */}
+          <div className="flex-1 relative">
+            <img src="/kids.png" alt="Enjoy on your TV" className="mt-4" />
+          </div>
+          {/* right */}
+          <div className="flex-1 text-center md:text-left">
+            <h2 className="text-4xl md:text-5xl font-extrabold mb-4">
+              Create profiles for kids
+            </h2>
+            <p className="text-lg md:text-xl">
+              Send kids on adventures with their favorite characters in a space
+              made just for them—free with your membership.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
-//3:20
+
 export default AuthScreen;
