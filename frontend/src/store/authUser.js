36668858ff6cd2ec02cf41ce7f0a545b80fe2899
Santii05-C <santi.c.pgm@gmail.com>
2025-04-12 23:08:23 -0300

    last commit

--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -6,6 +6,8 @@ export const useAuthStore = create((set) => ({
   user: null,
   isSigninUp: false,
   isCheckingAuth: true,
+  isLoggingOut: false,
+  isLoggingIn: false,
   signup: async (credentials) => {
     set({ isSigningUp: true });
     try {
@@ -17,8 +19,27 @@ export const useAuthStore = create((set) => ({
       set({ isSigningUp: false, user: null });
     }
   },
-  login: async () => {},
-  logout: async () => {},
+  login: async (credentials) => {
+    set({ isLoggingIn: true });
+    try {
+      const response = await axios.post("/api/v1/auth/login", credentials);
+      set({ user: response.data.user, isLoggingIn: false });
+    } catch (error) {
+      set({ isLoggingIn: false, user: null });
+      toast.error(error.response.data.message || "Login failed");
+    }
+  },
+  logout: async () => {
+    set({ isLoggingOut: true });
+    try {
+      await axios.post("/api/v1/auth/logout");
+      set({ user: null, isLoggingOut: false });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      set({ isLoggingOut: false });
+      toast.error(error.response.data.message || "Logout failed");
+    }
+  },
   authCheck: async () => {
     set({ isCheckingAuth: true });
     try {
@@ -27,7 +48,7 @@ export const useAuthStore = create((set) => ({
       set({ user: response.data.user, isCheckingAuth: false });
     } catch (error) {
       set({ isCheckingAuth: false, user: null });
-      toast.error(error.response.data.message || "An error occurred");
+      // toast.error(error.response.data.message || "An error occurred");
     }
   },
 }));
