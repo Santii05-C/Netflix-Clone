9c5926ddebc61f3f1ce58946be8fe7b1f30ef50e
Santii05-C <santi.c.pgm@gmail.com>
2025-04-13 23:13:46 -0300

    last commit

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -11,10 +11,12 @@ const HomeScreen = () => {
           alt="Hero img"
           className="absolute top-0 left-0 w-full h-full object-cover -z-50"
         />
+
         <div
           className="absolute top-0 left-0 w-full h-full bg-black/50 -z-50"
           aria-hidden="true"
         />
+
         <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32">
           <div
             className="bg-gradient-to-b from-black via-transparent to-transparent 
